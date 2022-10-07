e406b59b0cd5ebb20471f1c4b228d93a59243584
Stefan Djordjevic <djordjeviczstefan@gmail.com>
2022-10-07 13:00:42 +0200

    map1

--- src/components/MapFlet/basic.js
+++ src/components/MapFlet/basic.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer,M  } from "react-leaflet";
+import { MapContainer as MyContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import osm from "./osm-providers";
 import { useRef } from "react";
 import "leaflet/dist/leaflet.css";
@@ -18,7 +18,7 @@ export const BasicMap = () => {
   });
   
   return (
-    <MapContainer center={position} zoom={16} scrollWheelZoom={false}>
+    <MyContainer center={position} zoom={16} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -30,6 +30,6 @@ export const BasicMap = () => {
       <Marker position={position1} icon={iconLocation}>
         <Popup>Nasa lokacija</Popup>
       </Marker>
-    </MapContainer>
+    </MyContainer>
   );
 };
